21dce8c3da60bb11dd6cfe1c98b62b975016d2ee
Ticean Bennett <ticean@gmail.com>
2011-06-03 21:42:05 -0700

    Panel is in admin.

---
+++ app/code/community/ScaleWorks/Bitcoin/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ScaleWorks Bitcoin Data Helper
+ *
+ * @category   ScaleWorks
+ * @package    ScaleWorks_Bitcoin
+ * @author     ScaleWorks <develop@scaleworks.co>
+ */
+class ScaleWorks_Bitcoin_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
