ceb2f48b95318b9549a3074cc99677299177caf8
Ticean Bennett <ticean@gmail.com>
2011-06-08 01:07:15 -0700

    Added getNewAddress() method.

--- app/code/community/ScaleWorks/Bitcoin/Model/Bitcoin.php
+++ app/code/community/ScaleWorks/Bitcoin/Model/Bitcoin.php
@@ -41,4 +41,22 @@ class ScaleWorks_Bitcoin_Model_Bitcoin extends Varien_Object
         return $this->getClient()->getinfo();
     }
 
+    /**
+     * Returns a new bitcoin address for receiving payments.
+     *
+     * If $account is specified (recommended), it is added to the address book so
+     * payments received with the address will be credited to $account.
+     *
+     * @param string $account Label to apply to the new address
+     * @return string Bitcoin address
+     * @throws BitcoinClientException
+     */
+    public function getNewAddress($account = NULL) {
+      if (!$account || empty($account)) {
+        return $this->getClient()->getnewaddress();
+      }
+      return $this->getClient()->getnewaddress($account);
+    }
+
+
 }
