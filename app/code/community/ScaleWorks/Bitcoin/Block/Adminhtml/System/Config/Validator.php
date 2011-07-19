bd758110af645bfd5ac4fcc75a190808a69f3841
Ticean Bennett <ticean@gmail.com>
2011-07-19 01:33:01 -0700

    Added bitcoind validator button to admin panel.
    Button just opens a stub url. Still need to add a real bitcoin validation
    controller.

---
+++ app/code/community/ScaleWorks/Bitcoin/Block/Adminhtml/System/Config/Validator.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * ScaleWorks Bitcoin
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@scaleworks.co so we can send you a copy immediately.
+ *
+ * @category    ScaleWorks
+ * @package     ScaleWorks_Bitcoin
+ * @copyright   Copyright (c) 2011 Ticean Bennett (http://www.scaleworks.co)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Custom renderer for Bitcoind validation wizard popup.
+ *
+ * @category    ScaleWorks
+ * @package     ScaleWorks_Bitcoin
+ * @author      Ticean Bennett <ticean@gmail.com>
+ */
+class ScaleWorks_Bitcoin_Block_Adminhtml_System_Config_Validator extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    /**
+     * Set template to itself
+     */
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        if (!$this->getTemplate()) {
+            $this->setTemplate('scaleworks/bitcoin/system/config/validator.phtml');
+        }
+        return $this;
+    }
+
+    /**
+     * Unset some non-related element parameters
+     *
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {
+        $element->unsScope()->unsCanUseWebsiteValue()->unsCanUseDefaultValue();
+        return parent::render($element);
+    }
+
+    /**
+     * Get the button and scripts contents
+     *
+     * @param Varien_Data_Form_Element_Abstract $element
+     * @return string
+     */
+    protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
+    {
+        $originalData = $element->getOriginalData();
+        $this->addData(array(
+            'button_label' => Mage::helper('bitcoin')->__($originalData['button_label']),
+            'button_url'   => $originalData['button_url'],
+            'html_id' => $element->getHtmlId(),
+        ));
+        return $this->_toHtml();
+    }
+}
